d1613591fcc5da9993db5060eeef6cdf2a79daaf
Avinash <sharma.avinash96@gmail.com>
2020-08-05 18:52:18 +0530

    Reading values into the array

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -13,3 +13,6 @@ dictionary[compute1]=$comp1
 dictionary[compute2]=$comp2
 dictionary[compute3]=$comp3
 dictionary[compute4]=$comp4
+
+arr=(`printf '%s\n' "${dictionary[@]}" | sort -n`)
+echo ${arr[@]}
