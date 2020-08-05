1446f5a64f3044ca63d5696bb9a4f340c13c70b1
Avinash <sharma.avinash96@gmail.com>
2020-08-05 18:48:23 +0530

    storing computation result in Dictonary

--- SortingArithmaticComputation.sh
+++ SortingArithmaticComputation.sh
@@ -8,3 +8,8 @@ comp1=$((a+b*c))
 comp2=$((a*b+c))
 comp3=$((c+a/b))
 comp4=$((a%b+c))
+declare -A dictionary
+dictionary[compute1]=$comp1
+dictionary[compute2]=$comp2
+dictionary[compute3]=$comp3
+dictionary[compute4]=$comp4
